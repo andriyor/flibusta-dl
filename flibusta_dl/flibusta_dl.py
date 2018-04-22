4813107b91978063529c923f81b2bac0d77f1239
Andriy Orehov <andriyorehov@gmail.com>
2018-04-22 14:58:37 +0300

    add asynchronous downloading using grequests

--- flibusta_dl/flibusta_dl.py
+++ flibusta_dl/flibusta_dl.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import time
 import cgi
 import pathlib
 import zipfile
 
 import click
 import requests
+import grequests
 import humanize
 from pyquery import PyQuery as pq
 from tqdm import tqdm
@@ -26,7 +28,12 @@ def get_search_result(book_name, sort):
     except requests.exceptions.ConnectionError:
         click.echo('не удалось подключиться к серверу flibusta.is')
         sys.exit(1)
-    return r.text
+
+    if r.text == 'Не нашлось ни единой книги, удовлетворяющей вашим требованиям.':
+        tqdm.write(f'Не нашлось ни единой книги по запросу {book_name}')
+        return 'No result'
+    else:
+        return r.text
 
 
 def fetch_book_id(search_result, sort):
@@ -41,6 +48,53 @@ def fetch_book_id(search_result, sort):
     return book
 
 
+def get_all_links(books, sort, file_format):
+    books_link = dict()
+    for book_name in books:
+        search_result = get_search_result(book_name, sort)
+        if search_result == 'No result':
+            continue
+        else:
+            book = fetch_book_id(search_result, sort)
+            link = f'http://flibusta.is{book}/{file_format}'
+            books_link[book_name] = link
+    return books_link
+
+
+def save_file(sfn, book_file, output_folder, file_format, book_name):
+    if sfn:
+        filename = cgi.parse_header(book_file.headers['content-disposition'])[1]['filename']
+        file_path = os.path.join(output_folder, filename)
+    else:
+        file_path = os.path.join(output_folder, f'{book_name}.{file_format}')
+
+    with open(file_path, 'wb') as f:
+        f.write(book_file.content)
+
+    if file_format == 'fb2':
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(output_folder)
+        os.remove(file_path)
+
+
+def download_async(books_link, books, sfn, output_folder, file_format):
+    start_time = time.time()
+    downloaded_sizes, downloaded_book = [], []
+    rs = (grequests.get(u) for u in books_link.values())
+    for book_file, book_name in zip(grequests.map(rs, size=3), books_link.keys()):
+        save_file(sfn, book_file, output_folder, file_format, book_name)
+
+        content_length = int(book_file.headers['content-length'])
+        downloaded_sizes.append(content_length)
+        downloaded_book.append(book_name)
+        size = humanize.naturalsize(content_length)
+        click.echo(f'книга {book_name} загружена размер - {size}')
+
+    total_size = humanize.naturalsize(sum(downloaded_sizes))
+    end_time = humanize.naturaldelta(time.time() - start_time)
+    click.echo(f'всего загружено {len(downloaded_book)} книг из {len(books)} общим размером - {total_size} за {end_time}')
+
+
 @click.command()
 @click.argument('infile', type=click.File('r'))
 @click.option('-o', '--output_folder', default='flibusta_books', help='путь к папке в которую будут сохранятся книги')
@@ -52,38 +106,34 @@ def fetch_book_id(search_result, sort):
 @click.option('-r', '--rating', 'sort', flag_value='rating', help='приоритет загрузки по оценке')
 @click.option('-ff', '--file_format', type=click.Choice(['fb2', 'epub', 'mobi']), default='epub', help=' формат книг')
 @click.option('--sfn', is_flag=True, help='имя файла такое же как при загрузке с сайта')
-def cli(infile, output_folder, sort, file_format, sfn):
+@click.option('--asy', is_flag=True, help='асинхронная загрузка книг')
+def cli(infile, output_folder, sort, file_format, sfn, asy):
     books = infile.read().splitlines()
-    downloaded_sizes, downloaded_book = [], []
-    for book_name in tqdm(books, miniters=1):
-        search_result = get_search_result(book_name, sort)
+    pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
 
-        if search_result == 'Не нашлось ни единой книги, удовлетворяющей вашим требованиям.':
-            tqdm.write(f'Не нашлось ни единой книги по запросу {book_name}')
-            continue
+    if asy:
+        books_link = get_all_links(books, sort, file_format)
+        download_async(books_link, books, sfn, output_folder, file_format)
 
-        book = fetch_book_id(search_result, sort)
-        book_file = requests.get(f'http://flibusta.is{book}/{file_format}')
+    else:
+        start_time = time.time()
+        downloaded_sizes, downloaded_book = [], []
+        for book_name in tqdm(books, miniters=1):
+            search_result = get_search_result(book_name, sort)
 
-        pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True) 
-        if sfn:
-            filename = cgi.parse_header(book_file.headers['content-disposition'])[1]['filename']
-            file_path = os.path.join(output_folder, filename)
-        else:
-            file_path = os.path.join(output_folder, f'{book_name}.{file_format}')
+            if search_result == 'No result':
+                continue
 
-        with open(file_path, 'wb') as f:
-            f.write(book_file.content)
+            book = fetch_book_id(search_result, sort)
+            book_file = requests.get(f'http://flibusta.is{book}/{file_format}')
+            save_file(sfn, book_file, output_folder, file_format, book_name)
+
             content_length = int(book_file.headers['content-length'])
             downloaded_sizes.append(content_length)
             downloaded_book.append(book_name)
             size = humanize.naturalsize(content_length)
             tqdm.write(f'книга {book_name} загружена размер - {size}')
 
-        if file_format == 'fb2':
-            with zipfile.ZipFile(file_path, 'r') as zip_ref:
-                zip_ref.extractall(output_folder)
-            os.remove(file_path)
-
-    total_size = humanize.naturalsize(sum(downloaded_sizes))
-    click.echo(f'всего загружено {len(downloaded_book)} книг из {len(books)} общим размером - {total_size}')
+        total_size = humanize.naturalsize(sum(downloaded_sizes))
+        end_time = humanize.naturaldelta(time.time() - start_time)
+        click.echo(f'всего загружено {len(downloaded_book)} книг из {len(books)} общим размером - {total_size} за {end_time}')
